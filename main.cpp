d41512073db195d3e04728c05797d33d607096f0
serkomma <serkomma@yandex.ru>
2023-11-29 03:19:30 +0300

    Refactoring, classes creating

--- main.cpp
+++ main.cpp
@@ -1,105 +1,164 @@
-#include "main.h"
+ #include "main.h"
 //----------------------------
-const std::string GAME_NAME = "Fly";
+
 //-------------------------------------
 int main()
 {   
     // Window
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), GAME_NAME);
-
-    // Sky
-    sf::Texture textureSky;
-    textureSky.loadFromFile(SKY_IMG);
-    sf::RectangleShape gamebackground(sf::Vector2f(WIDTH, HEIGHT));
-    gamebackground.setTexture(&textureSky);
-
-    //Player
-    Player player;
-    player.draw();
-    sf::Vector2f playerPos;
+    // Initial game object
+    Game game; 
+    game.init_game();
 
     while (window.isOpen())
     {
-        auto time = clock1.getElapsedTime().asSeconds();
-        auto timePlayer = time*PLAYER_SPEED;
-        clock1.restart();
-
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            switch (event.type)
-            {   // Keys
-                case sf::Event::KeyPressed:
-                            if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)    playerPos.y = -1 * timePlayer;
-                            if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)  playerPos.x = -1 * timePlayer;
-                            if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)  playerPos.y = timePlayer;
-                            if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) playerPos.x = timePlayer;
-                    break;
-                case sf::Event::KeyReleased:
-                            if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)    playerPos.y = 0;
-                            if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)  playerPos.x = 0;
-                            if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)  playerPos.y = 0;
-                            if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) playerPos.x = 0;
-                    break;
-            }
+            game.process_events(event);
         }
+        game.get_clock().restart();
 
         window.clear();
-        window.draw(gamebackground);
-        player.move(playerPos);
-        window.draw(player.get_object());
+        // Background objects
+        for (auto bg : game.background_objects)
+            window.draw((*bg).get_object());
+        // Player object
+        game.player.move();
+        window.draw(game.player.get_object());
+        // Aircraft object
+        for (auto ac : game.aircraft_objects)
+            window.draw((*ac).get_object());
+
         window.display();
     }
 
     return 0;
+};
+
+Game::Game(/* args */)
+{
 }
 
-GameObject::GameObject(){
+Game::~Game()
+{
+}
+
+void Game::init_game(){
+    // Sky
+    background_objects.push_back(new GameObject(GAME_OBJECTS.at("sky"), sf::Vector2f(0,0)));
+    //Player
+    Player player;
+}
+sf::Clock & Game::get_clock(){
+    return clock;
+}
+
+void Game::process_events(sf::Event event){
+    switch (event.type)
+        {   // Keys
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)    player.set_motion_y(-1 * get_time(PLAYER_SPEED));
+                if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)  player.set_motion_x(-1 * get_time(PLAYER_SPEED));
+                if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)  player.set_motion_y(get_time(PLAYER_SPEED));
+                if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) player.set_motion_x(get_time(PLAYER_SPEED));
+                break;
+            case sf::Event::KeyReleased:
+                if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)    player.set_motion_y(0);
+                if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)  player.set_motion_x(0);
+                if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)  player.set_motion_y(0);
+                if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) player.set_motion_x(0);
+                break;
+        }
+};
+
+float Game::get_time(int multiplicator){
+    return clock.getElapsedTime().asSeconds() * multiplicator;
+}
+
+GameObject::GameObject(game_object game_object, sf::Vector2f position){
+    data = game_object;
+    switch (game_object.draw_type)
+    {
+    case Draw_types::rectangle:
+        texture.loadFromFile(game_object.img);
+        object = new sf::RectangleShape(sf::Vector2f(game_object.width, game_object.height));
+        rect_object = (sf::RectangleShape*) (object);
+        (*rect_object).setTexture(&texture);
+        (*rect_object).setPosition(position);
+        object = rect_object;
+        rect_object = nullptr;
+        break;
+    
+    default:
+        break;
+    }
 }
 
 GameObject::~GameObject(){
 }
 
-sf::RectangleShape & GameObject::get_object(){
+
+void GameObject::move(){
+    switch (data.draw_type)
+    {
+    case Draw_types::rectangle:
+        rect_object = (sf::RectangleShape*) (object);
+        (*rect_object).move(motion);
+        object = rect_object;
+        rect_object = nullptr;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void GameObject::set_motion_x(float xmotion){
+    this->motion.x = xmotion;
+}
+
+void GameObject::set_motion_y(float ymotion){
+    this->motion.y = ymotion;
+}
+
+sf::Drawable & GameObject::get_object(){
     return *object;
 }
 
-Player::Player(/* args */){
-}
-
-Player::~Player(){
-}
-
-void Player::draw(){
-    texture.loadFromFile(PLAYER_IMG);
-    object = new sf::RectangleShape(sf::Vector2f(100, 40));
-    (*object).setTexture(&texture);
-    (*object).setPosition(sf::Vector2f(MIN_WIDTH,(MAX_HEIGHT + MIN_HEIGHT)/2));
-    
-}
-
-void Player::move(sf::Vector2f bias){
+void Player::move(){
     // Player control
-    (*object).move(bias);
+    rect_object = (sf::RectangleShape*) (object);
+    (*rect_object).move(motion);
     // Borders
-    auto position = (*object).getPosition();
+    auto position = (*rect_object).getPosition();
     if (position.x > MAX_WIDTH)
-        (*object).setPosition(MAX_WIDTH, position.y);
+        (*rect_object).setPosition(MAX_WIDTH, position.y);
     if (position.y > MAX_HEIGHT)
-        (*object).setPosition(position.x, MAX_HEIGHT);
+        (*rect_object).setPosition(position.x, MAX_HEIGHT);
     if (position.x < MIN_WIDTH)
-        (*object).setPosition(MIN_WIDTH, position.y);
+        (*rect_object).setPosition(MIN_WIDTH, position.y);
     if (position.y < MIN_HEIGHT)
-        (*object).setPosition(position.x, MIN_HEIGHT);
+        (*rect_object).setPosition(position.x, MIN_HEIGHT);
     if (position.x > MAX_WIDTH && position.y > MAX_HEIGHT)
-        (*object).setPosition(MAX_WIDTH, MAX_HEIGHT);
+        (*rect_object).setPosition(MAX_WIDTH, MAX_HEIGHT);
     if (position.x > MAX_WIDTH && position.y < MIN_HEIGHT)
-        (*object).setPosition(MAX_WIDTH, MIN_HEIGHT);
+        (*rect_object).setPosition(MAX_WIDTH, MIN_HEIGHT);
     if (position.x < MIN_WIDTH && position.y > MAX_HEIGHT)
-        (*object).setPosition(MIN_WIDTH, MAX_HEIGHT);
+        (*rect_object).setPosition(MIN_WIDTH, MAX_HEIGHT);
     if (position.x < MIN_WIDTH && position.y < MIN_HEIGHT)
-        (*object).setPosition(MIN_WIDTH, MIN_HEIGHT);
+        (*rect_object).setPosition(MIN_WIDTH, MIN_HEIGHT);
+    object = rect_object;
+    rect_object = nullptr;
 }
+
+// Aircraft::Aircraft()
+// {
+// }
+
+// Aircraft::~Aircraft()
+// {
+// }
--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 // Screen
-// const std::string GAME_NAME = "Fly";
+const std::string GAME_NAME = "Fly";
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
@@ -14,43 +14,97 @@ const int MAX_HEIGHT = HEIGHT - 120;
 const int MIN_WIDTH = 40;
 const int MAX_WIDTH = WIDTH - 120;
 
-// Media
-const std::string SKY_IMG = "Images/sky.jpg";
-const std::string PLAYER_IMG = "Images/player.png";
-
 // Player
 const int PLAYER_SPEED = 600;
 
-// Time
-sf::Clock clock1;
+// Types of draw objects
+enum class Draw_types{
+    rectangle, sprite
+};
+
+//Aircrafts
+struct game_object{
+    int width;
+    int height;
+    int speed;
+    std::string img;
+    std::string img_add;
+    Draw_types draw_type;
+};
+
+const std::map<std::string, game_object> GAME_OBJECTS{
+    {"player",     { 100,   40,     800, "Images/player.png",             "",                                  Draw_types::rectangle}},
+    {"sky",        { WIDTH, HEIGHT, 800, "Images/sky.jpg",                "",                                  Draw_types::rectangle}},
+    {"boeing",     { 100,   40,     800, "Images/plane_boeing_left.png",  "Images/plane_boeing_right.png",     Draw_types::rectangle}},
+    {"four_wings", { 100,   40,     200, "Images/plane_boeing_left.png",  "Images/plane_four_wings_right.png", Draw_types::rectangle}},
+    {"fighter",    { 100,   40,     800, "Images/plane_fighter_left.png", "Images/plane_fighter_right.png",    Draw_types::rectangle}},
+    {"baloon1",    { 100,   40,     50,  "Images/baloon_1_left.png",      "",                                  Draw_types::rectangle}},
+    };
 
 // Logic classes
-// Common abstract class for all objects
+class Game;
+class GameObject;
+class Player;
+
+// Common class for all objects in the game
 class GameObject
 {
 protected:
+    game_object data;
     sf::Vector2f position;
     sf::Texture texture;
-    sf::RectangleShape *object;
+    sf::Drawable *object;
+    sf::RectangleShape *rect_object;
+    sf::Vector2f motion;
+    // void draw();
 public:
-    GameObject();
+    GameObject(game_object game_object, sf::Vector2f position);
     ~GameObject();
-    virtual void draw() = 0;
-    virtual void move(sf::Vector2f bias) = 0;
-    sf::RectangleShape & get_object();
+    virtual void move();
+    sf::Drawable & get_object();
+    void set_motion_x(float xmotion);
+    void set_motion_y(float ymotion);
 };
 // Class describes player
 class Player : public GameObject
 {
 private:
-    /* data */
+
 public:
-    Player(/* args */);
-    ~Player();
-    void draw() override;
-    void move(sf::Vector2f bias) override;
+    Player() : GameObject(GAME_OBJECTS.at("player"), sf::Vector2f(MIN_WIDTH,(MAX_HEIGHT + MIN_HEIGHT)/2)){ };
+    ~Player() {delete object, rect_object;};
+    // void draw() override;
+    void move() override;
+};
+// Common class
+class Game
+{
+private:
+    // Time
+    sf::Clock clock;
+public:
+    Player player;
+    std::vector<GameObject*> background_objects;
+    std::vector<GameObject*> aircraft_objects;
+    GameObject * q;
+    Game(/* args */);
+    ~Game();
+    void init_game();
+    sf::Clock & get_clock();
+    float get_time(int multiplicator);
+    void process_events(sf::Event event);
 };
 
-int main();
-#endif
+// Class describes other aircrafts except player
+// class Aircraft : public GameObject
+// {
+// private:
+//     /* data */
+// public:
+//     Aircraft();
+//     ~Aircraft();
+// };
 
+int main();
+
+#endif
